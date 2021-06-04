e279b5cf7eb6a6693159acb5fa4f010f660d2c2f
hyomin632 <80568982+hyomin632@users.noreply.github.com>
2021-06-04 17:54:33 +0900

    springmvc + tiles + maven 성적 처리 적용

---
+++ spring/celeste/spring/mvc/TilesController.java
@@ -0,0 +1,29 @@
+package celeste.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TilesController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index.tiles";
+	}
+	
+	@RequestMapping("/board/list")
+	public String list() {
+		return "board/list.tiles";
+	}
+	
+	@RequestMapping("/board/view")
+	public String view() {
+		return "board/view.tiles";
+	}
+	
+	@RequestMapping("/board/write")
+	public String write() {
+		return "board/write.tiles";
+	}
+	
+}
---
+++ spring/celeste/spring/service/SungJukService.java
@@ -0,0 +1,16 @@
+package celeste.spring.service;
+
+import java.util.List;
+
+import celeste.spring.vo.SungJuk;
+
+public interface SungJukService {
+	int newSungJuk(SungJuk sj);
+	List<SungJuk> readAllSungJuk();
+	
+	SungJuk readOneSungJuk(int sjno);
+	int modifySungJuk();
+	int removeSungJuk();
+	
+	void computeSungJuk(SungJuk sj);
+}
